2570d62741ae5a2984109b643392a213b27ac42c
Henri Korhola <hkorhola@gmail.com>
2017-01-06 23:24:01 +0200

    refined script

--- reko.rb
+++ reko.rb
@@ -12,27 +12,35 @@ Aws.config.update({
 
 begin
   rekognition = Aws::Rekognition::Client.new(region: Aws.config[:region], credentials: Aws.config[:credentials])
-
+  
+  if ARGV.length < 1
+    abort( "Please give an image file name as argument and try again")
+  else  
+    img = File.read(ARGV.first)     
+  end
+    
   #detect faces  
   resp = rekognition.detect_faces({
-    image: { 
-      bytes: File.read(ARGV.first) 
+    image: {
+      bytes: img 
     },
     attributes: ["ALL"], # What attributes to return
   })
   
   resp.face_details[0].emotions.each do |emo|
-    puts emo.type + " " + emo.confidence.to_i.to_s #=> String, one of "HAPPY", "SAD", "ANGRY", "CONFUSED", "DISGUSTED", "SURPRISED", "CALM", "UNKNOWN"
+    puts emo.type + " " + emo.confidence.to_i.to_s #=> Strings "HAPPY", "SAD", "ANGRY", "CONFUSED", "DISGUSTED", "SURPRISED", "CALM", "UNKNOWN"
   end
   
   
   #get labels of image
-  resp2 = rekognition.detect_labels(
-          image: { bytes: File.read(ARGV.first) }
+  resp = rekognition.detect_labels(
+    image: { 
+      bytes: img 
+    }
   )
     
-  resp2.labels.each do |l|
-    puts "#{l.name} #{l.confidence.to_i}"
+  resp.labels.each do |lb|
+    puts "#{lb.name} #{lb.confidence.to_i}"
   end
   
 rescue Aws::Rekognition::Errors::ServiceError => e
